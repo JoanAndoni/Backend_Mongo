cd7ff0e5cf17d40fa28ef16b92a24ce633b63450
JoanAndoni <mcr_joan@hotmail.com>
2020-05-23 19:50:28 -0500

    Passport adn ES6 with model

---
+++ config/database.js
@@ -0,0 +1,2 @@
+export const database = 'mongodb+srv://user:imgoingtograduate@documentsblockchain-yuhgo.gcp.mongodb.net/test?retryWrites=true&w=majority';
+export const secret = '';
---
+++ config/passport.js
@@ -0,0 +1,5 @@
+import passportJWT from 'passport-jwt';
+import user from '../models/user';
+import * as config from './database';
+
+export default function (passport) { }
